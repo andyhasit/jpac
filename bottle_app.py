8e2929827894df1d8d3473635fe40dd9c3707d64
Andrew Buchan <andyhasit@gmail.com>
2018-12-18 19:41:56 +0000

    Implemented rout wrapper and transactions

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,5 @@
-import json
-import os
-from bottle import default_app, route, template, static_file, request, response, auth_basic
+from bottle import default_app, route, template, static_file, request, response,\
+    HTTPError
 from json_storage import ApiError, MyJsonStorageHandler
 
 
@@ -30,24 +29,51 @@ def server_static(filename):
     return static_file(filename, root='/static')
 
 
-@route('/')
-def index():
-    return template('index')
-
 @route('/', method = 'OPTIONS')
 @route('/<path:path>', method = 'OPTIONS')
 def options_handler(path = None):
     return
 
 
-@route('/actions', method='POST')
-@auth_basic(check)
-def actions():
-    data = request.json
-    storage = MyJsonStorageHandler()
-    storage.set_paths(JSON_BASE, 'andyhasit_pointy_v2')
+@route('/')
+def index():
+    return template('index')
+
+
+storage_containers = {}
+
+@route('/<app>/actions', method='POST')
+def actions(app):
+    return wrap_storage_call(request, app, 'do_actions', request.json)
+
+
+def get_storage(app, user):
+    if user != 'tim':
+        raise HTTPError(401, 'Invalid login')
+    key = (app, user)
+    if key in storage_containers:
+        storage = storage_containers[key]
+    else:
+        storage = MyJsonStorageHandler()
+        storage.set_paths(JSON_BASE, '{}____{}'.format(*key))
+        storage_containers[key] = storage
+    return storage
+
+
+def wrap_storage_call(request, app, method, params):
+    """
+    Generic wrapper for all storage calls.
+    """
     try:
-        result = storage.do_actions(**data)
+        user, password = request.auth or (None, None)
+
+        '''
+        err = HTTPError(401, text)
+        err.add_header('WWW-Authenticate','')
+        '''
+        #TODO: wrap in try execpt which validates user too
+        storage = get_storage(app, user)
+        result = getattr(storage, method)(**params)
         return {
             "status" : "success",
             "data" : result
@@ -61,6 +87,8 @@ def actions():
                 "data": e.data
             }
         }
+    except HTTPError as e:
+        return e
     except BaseException as e:
         return {
             "status" : "error",
--- json_storage.py
+++ json_storage.py
@@ -10,23 +10,94 @@ We track the , which the client can use to determine if its data is dirty.
 
 """
 import os
+import datetime
 import ujson
+
+
+class ApiError(Exception):
+
+    def __init__(self, code="uncaught", msg="", data=None):
+        super(Exception, self).__init__('{} -- {}'.format(code, msg))
+        self.code = code
+        self.data = data
 
 
 class JsonTwoFileStorageHandler():
     """
+    Transaction aware json database. Allows only one transaction.
+
     Methods for working with two-file system:
         xyz_data_db.json  --  the data object which is entirely modifiable by clients
         xyz_meta_db.json  --  meta data such as the last_id and revision.
 
 
     """
+    def __init__(self):
+        self._must_reload = True
+        self.transaction_id = None
+        self.transaction_initiated = None
+        self.transaction_timeout = None
+        self.data_db = None
+        self.meta_db = None
+        self._data_db_path = None
+        self._meta_db_path = None
 
     def set_paths(self, base_path, database_name):
-        self.database_name = database_name
-        self.base_path = base_path
-        self.data_db_path = self._get_db_path(database_name, 'data_db')
-        self.meta_db_path = self._get_db_path(database_name, 'meta_db')
+        self._data_db_path = self._get_db_path(base_path, database_name, 'data_db')
+        self._meta_db_path = self._get_db_path(base_path, database_name, 'meta_db')
+
+    def load(self):
+        if self.transaction_id is not None:
+            now = datetime.datetime.now()
+            timeout = self.transaction_initiated + datetime.timedelta(seconds=self.transaction_timeout)
+            if timeout > now:
+                self._must_reload = True
+
+        if self._must_reload:
+            self.data_db = self._load_json(self._data_db_path)
+            self.meta_db = self._load_json(self._meta_db_path)
+            if self.meta_db == {}:
+                self.meta_db = {'rev': 0, 'last_id': 0}
+            self._must_reload = False
+
+    def save(self):
+        self._save_json(self._data_db_path, self.data_db)
+        self._save_json(self._meta_db_path, self.meta_db)
+
+    def start_transaction(self, timeout=5):
+        transaction_id = 100
+        self.transaction_id = transaction_id
+        self.transaction_initiated = datetime.datetime.now()
+        self.transaction_timeout = timeout
+        return transaction_id
+
+    def commit_transaction(self, transaction_id):
+        if transaction_id == self.transaction_id:
+            self.save()
+        self.transaction_id = None
+
+    def abort_transaction(self, transaction_id):
+        if transaction_id == self.transaction_id:
+            self._must_reload = True
+        self.transaction_id = None
+
+    def check_transaction(self, transaction_id):
+        if transaction_id is None and self.transaction_id is None:
+            # No transaction expected or in progress, all good
+            return
+        if transaction_id is None and self.transaction_id is not None:
+            raise ApiError(code='transaction_in_progress')
+        if transaction_id is not None and self.transaction_id is None:
+            raise ApiError(code='no_transaction_in_progress')
+        if transaction_id != self.transaction_id:
+            raise ApiError(code='transaction_id_mismatch')
+
+    def check_revision(self, revision):
+        if int(revision) != self.meta_db['rev']:
+            raise ApiError(code='revision_mismatch', data={
+                'client_revision': int(revision),
+                'server_revision': self.meta_db['rev'],
+                })
 
     def _save_json(self, filepath, data):
         with open(filepath, 'w') as fp:
@@ -38,41 +109,22 @@ class JsonTwoFileStorageHandler():
         with open(filepath) as fp:
             return ujson.load(fp)
 
-    def _get_db_path(self, database_name, suffix):
-        return os.path.join(self.base_path, '{}_{}.json'.format(database_name, suffix))
-
-    def load(self):
-        self.data_db = self._load_json(self.data_db_path)
-        self.meta_db = self._load_json(self.meta_db_path)
-        if self.meta_db == {}:
-            self.meta_db = {'rev': 0, 'last_id': 0}
-
-    def save(self):
-        self._save_json(self.data_db_path, self.data_db)
-        self._save_json(self.meta_db_path, self.meta_db)
-
-
-class ApiError(Exception):
-
-    def __init__(self, code="uncaught", msg="", data=None):
-        super(Exception, self).__init__('{} -- {}'.format(code, msg))
-        self.code = code
-        self.data = data
+    def _get_db_path(self, base_path, database_name, suffix):
+        return os.path.join(base_path, '{}_{}.json'.format(database_name, suffix))
 
 
 class ActionsMixin():
     valid_actions = ('create', 'read', 'update', 'delete')
 
-    def do_actions(self, revision, action_sets):
+    def do_actions(self, revision, action_sets, transaction_id=None):
         """
-        Pass action_sets which define what changes to make.
+        @revision: the client stored revision
+        @action_sets: changes to make
+        @transaction_id: the id of the pending transaction if there is one
         """
         self.load()
-        if int(revision) != self.meta_db['rev']:
-            raise ApiError(code='revision_mismatch', data={
-                'client_revision': int(revision),
-                'server_revision': self.meta_db['rev'],
-                })
+        self.check_transaction(transaction_id)
+        self.check_revision(revision)
         result = {}
         for action_type, actions in action_sets.items():
             if action_type not in self.valid_actions:
@@ -84,7 +136,8 @@ class ActionsMixin():
                 action_results[key] = getattr(self, '_' + action_type)(**params)
             result[action_type] = action_results
         result['revision'] = self.meta_db['rev']
-        self.save()
+        if transaction_id is None:
+            self.save()
         return result
 
     def _drill(self, path):
